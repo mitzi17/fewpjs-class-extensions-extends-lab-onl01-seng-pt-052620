a1dc10e2020059b3381b709514228e604f62fb6c
Mitzi17 <m_estrada17@live.com>
2020-12-25 20:36:10 -0800

    commit tree d16b37429768808401dea61327080b7d05b8c49b

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+class Polygon {
+    constructor(arraySides) {
+        this.arraySides = arraySides
+        this.count = this.arraySides.length
+    }
+
+    
+    get countSides() {
+        return this.arraySides.length
+    }
+
+    get perimeter() {
+        if (!Array.isArray(this.arraySides)) return;
+        let sum = 0;
+        for (var int of this.arraySides) {
+        sum += int
+        }
+        return sum
+        }
+    }
+
+    class Triangle extends Polygon {
+        get isValid() {
+            if (!Array.isArray(this.arraySides)) return;
+            if (this.count !== 3) return;
+                let side1 = this.arraySides[0]
+                let side2 = this.arraySides[1]
+                let side3 = this.arraySides[2]
+                return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+          }
+    }
+
+    class Square extends Polygon {
+
+        get isValid() {
+          if (!Array.isArray(this.arraySides)) return;
+          if (this.count !== 4) return;
+          let side1 = this.arraySides[0]
+          let side2 = this.arraySides[1]
+          let side3 = this.arraySides[2]
+          let side4 = this.arraySides[3]
+          return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+        }
+      
+        get area() {
+          if (!Array.isArray(this.arraySides)) return;
+          if (this.count !== 4) return;
+          return this.arraySides[0] * this.arraySides[0]
+        }
+      }
